4da702d16c390a90c2d6294d054dc1118af86e94
olena4reunited <olenaduringintermission@gmail.com>
2024-09-21 12:45:15 +0300

    build: create index view and set templates: base, header, footer, sidebar and index

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,53 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import SET_NULL
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("urgent", "Urgent"),
+        ("high", "High"),
+        ("medium", "Medium"),
+        ("low", "Low"),
+    ]
+
+    name = models.CharField(max_length=63, unique=True)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=63,
+        choices=PRIORITY_CHOICES,
+        default="medium"
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=SET_NULL
+    )
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    tasks = Task.objects.select_related("task_type").prefetch_related("assignees").filter("is_completed=False")
+
+    context = {
+        tasks: tasks,
+    }
+
+    return render(request=request, template_name="task_manager/index.html", context=context)
+
+
+
