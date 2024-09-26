418e1f7238ea16de4c4266e269aad9141f76346a
olena4reunited <olenaduringintermission@gmail.com>
2024-09-26 08:47:29 +0300

    build: add secret_key to env file

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -23,10 +23,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-4elzt#1slh2+sbc1$pcg2zf!8!9%ws@(8(@6b#3wng6)pkqbus"
-)
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
